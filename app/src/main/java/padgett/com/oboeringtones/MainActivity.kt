5eb032a5459aee584038c90c854776a2a40d2c9a
Jeff Padgett <jeff_padgett@hotmail.com>
2019-02-24 19:38:01 -0600

    - initial commit

---
+++ app/src/main/java/padgett/com/oboeringtones/MainActivity.kt
@@ -0,0 +1,13 @@
+package padgett.com.oboeringtones
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // what up
+    }
+}
